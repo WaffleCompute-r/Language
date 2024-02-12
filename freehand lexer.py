58513fbbcfe32c74db1587ab3d8fc98529782753
Arthur Norcott <66672400+boscotraps@users.noreply.github.com>
2024-02-12 16:00:29 +0000

    checking token types

--- freehand lexer.py
+++ freehand lexer.py
@@ -1,4 +1,10 @@
-class lexer:
+# Lexer
+
+class Lexer:
+    def __init__(self):
+        self.symbols = ["+", "-", "*", "/"]
+        self.keys = ['print', 'while', 'if', 'elif', 'else']
+
     #handles number tokens
     def lex_num(self, line):
         num=""
@@ -20,8 +26,6 @@ class lexer:
         # If we get here, the string was not closed
         raise SyntaxError("Close your strings, you fool!")
 
-    symbols = ["+", "-", "*", "/"]
-
     def lex_sym(self, line):
         symbol = ""
         for c in line:
@@ -32,14 +36,13 @@ class lexer:
         return 'SYM', symbol, len(symbol)
 
     def lex_id(self, line):
-        keys = ['print', 'while', 'if', 'elif', 'else']
         id = ""
         for c in line:
             if not c.isalpha():
                 break
             id += c
-        if id in keys:
-            return 'key', id, len(id)
+        if id in self.keys:
+            return 'KEY', id, len(id)
         else:
             return 'ID', id, len(id)
 
@@ -71,12 +74,61 @@ class lexer:
                 tokens.append((typ, tok))
             else:
                 count += 1
-        print(tokens)
+        return tokens
+    
+    # all token types: NUM, STR, SYM, ID, KEY
+    
+# Parser
+    
+class Parser:
+    def __init__(self, tokens):
+        # stores the tokens that the lexer is providing
+        self.tokens = tokens
+        # stores the current token that we are parsing
+        self.current_token = None
+        # stores the next token that we will parse
+        self.next_token()
+
+    def next_token(self):
+        try:
+            self.current_token = next(self.tokens)
+        # If we run out of tokens, set the current token to None
+        # if there is an error, the except block will run
+        except StopIteration:
+            self.current_token = None
+
+    def parse(self):
+        # set result to 0
+        result = 0
+
+        # while there are still tokens to parse
+        while self.current_token != None:
+            #if the current token is a number add it to the result
+            if self.current_token[0] == 'NUM':
+                # NUM is the type of token, and the second element is the actual token
+                result += int(self.current_token[1])
+            # if the current token is a symbol
+            if self.current_token[0] == 'SYM':
+                pass
+            # if the current token is a string
+            if self.current_token[0] == 'STR':
+                pass
+            # if the current token is an identifier
+            if self.current_token[0] == 'ID':
+                pass
+            # if the current token is a keyword
+            if self.current_token[0] == 'KEY':
+                pass
+            # otherwise raise an error
+            else:
+                raise SyntaxError("This token type is not in my list of valid token types")
+
+
 
 code = input("")
 
 # Create an instance of the lexer class
-lexer_instance = lexer()
+lexer_instance = Lexer()
 
 # Lex the user's code
 lexer_instance.lex(code)
